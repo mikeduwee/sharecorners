2ae987d9064b2aa005f54e2066d3f7905b4b2194
mikeduwee <mikeduwee@gmail.com>
2020-01-30 21:12:39 +0100

    Add fixmap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import logo from './img/logo.svg';
 import './App.css';
-import MapGL from 'react-map-gl';
-
-const MAPBOX_TOKEN = 'pk.eyJ1IjoibWlrZWR1d2VlIiwiYSI6ImNqbm00dndpZzA0NDczcm51b3Y5aG5uOXcifQ.N-ha9iC9A_qnLME7FtvSAg'; // Set your mapbox token here
+import Map from './Map'
 
 function App() {
-  const [viewport, setViewport] = useState({
-    latitude: 37.8,
-    longitude: -122.4,
-    zoom: 14,
-    bearing: 0,
-    pitch: 0
-  });
 
   return (
-    <MapGL
-      {...viewport}
-      width="100vw"
-      height="100vh"
-      mapStyle="mapbox://styles/mapbox/dark-v9"
-      onViewportChange={setViewport}
-      mapboxApiAccessToken={MAPBOX_TOKEN}
-    />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Foodie or what?
+          </p>
+          <Map></Map>
+      </header>
+    </div>
   );
 }
 
